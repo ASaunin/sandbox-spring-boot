57eacaa3877e11cfbc9c095db7c651c717989453
ASaunin <alsaunin@gmail.com>
2017-01-09 04:18:54 +0300

    Add profiles example

---
+++ profiles/main/java/com/config/Application.java
@@ -0,0 +1,24 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com")
+public class Application {
+
+    public static void main(String[] args) {
+        final ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        System.out.println("\nProfile properties example via components: " + context.getBean("componentDataSource"));
+
+        System.out.println("\nProfile properties example via beans: " + context.getBean("beanDataSource"));
+
+        System.out.println("\nEnvironment properties example using conditional annotation: " + context.getBean("emailService"));
+
+    }
+
+
+}
---
+++ profiles/main/java/com/config/db/bean/DataSource.java
@@ -0,0 +1,15 @@
+package com.config.db.bean;
+
+import lombok.Builder;
+import lombok.ToString;
+
+@Builder
+@ToString
+public class DataSource {
+
+    private String driver;
+    private String url;
+    private String username;
+    private String password;
+
+}
